313123e1063999631136ee23f8713d710e41792b
viktoriachernykh <chernycv@gmail.com>
2020-04-06 18:40:59 +0200

    changed productsList to functional component

--- src/components/HomePage/HomePageContainer.js
+++ src/components/HomePage/HomePageContainer.js
@@ -1,78 +1,37 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 
-import { fetchStores } from "../../store/store/actions";
-import { fetchProducts } from "../../store/product/actions";
+// import { fetchStores } from "../../store/store/actions";
+// import { fetchProducts } from "../../store/product/actions";
 
 import SearchProductInput from "./SearchProductInput";
 import ProductsList from "./ProductsList";
 // import StoresList from "./StoresList";
-import Map from "../Map/Map";
+// import Map from "../Map/Map";
 
-class HomePageContainer extends Component {
-  state = {
-    pageNumber: 1,
-  };
-
-  componentDidMount() {
-    this.props.fetchStores(this.state.pageNumber);
-    this.props.fetchProducts(this.state.pageNumber);
-  }
-
-  prevPage = () => {
-    if (this.state.pageNumber > 1) {
-      this.props.fetchProducts(this.state.pageNumber - 1);
-      this.setState({ pageNumber: this.state.pageNumber - 1 });
-    }
-  };
-
-  nextPage = () => {
-    if (this.props.productsLength > this.state.pageNumber * 10) {
-      this.props.fetchProducts(this.state.pageNumber + 1);
-      this.setState({ pageNumber: this.state.pageNumber + 1 });
-    }
-  };
-
+export default class HomePageContainer extends Component {
   render() {
-    const { products, stores } = this.props;
-
     return (
       <div>
         <SearchProductInput />
         <div className="container">
-          <ProductsList
-            products={products}
-            fetchProducts={this.props.fetchProducts}
-            prevPage={this.prevPage}
-            nextPage={this.nextPage}
-            pageNumber={this.state.pageNumber}
-          />
-          {stores ? <Map stores={stores} /> : "Loading map..."}
-          {/* <StoresList
-          stores={stores}
-          // fetchStores={this.props.fetchStores}
-          // prevPage={this.prevPage}
-          // nextPage={this.nextPage}
-          // pageNumber={this.state.pageNumber}
-        /> */}
+          <ProductsList />
+          {/* {stores ? <Map stores={stores} /> : "Loading map..."} */}
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    stores: state.stores.list.rows,
-    storesLength: state.stores.list.count,
-    products: state.products.list.rows,
-    productsLength: state.products.list.count,
-    user: state.session.user,
-    token: state.session.jwt,
-  };
-}
+// function mapStateToProps(state) {
+//   return {
+//     stores: state.stores.list.rows,
+//     storesLength: state.stores.list.count,
+//     products: state.products.list.rows,
+//     productsLength: state.products.list.count,
+//     user: state.session.user,
+//     token: state.session.jwt,
+//   };
+// }
 
-export default connect(mapStateToProps, {
-  fetchStores,
-  fetchProducts,
-})(HomePageContainer);
+// export default connect(mapStateToProps, {})(HomePageContainer);
