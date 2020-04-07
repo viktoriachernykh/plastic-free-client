99c2f641d90a0cefc0fd51cd7fd59280f62b0d34
viktoriachernykh <chernycv@gmail.com>
2020-04-07 13:34:08 +0200

    changed LoginForm to functional component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProductPageContainer from "./components/ProductPage/ProductPageContainer"
 import StorePageContainer from "./components/StorePage/StorePageContainer";
 import UserPageContainer from "./components/UserPage/UserPageContainer";
 import SignupFormContainer from "./components/Forms/Signup/SignupFormContainer";
-import LoginFormContainer from "./components/Forms/Login/LoginFormContainer";
+import LoginForm from "./components/Forms/LoginForm";
 import Logout from "./components/Forms/Logout/Logout";
 
 import "./styles/style.css";
@@ -23,7 +23,7 @@ class App extends Component {
           <Route exact path="/store/:id" component={StorePageContainer} />
           <Route exact path="/user/:id" component={UserPageContainer} />
           <Route exact path="/signup" component={SignupFormContainer} />
-          <Route exact path="/login" component={LoginFormContainer} />
+          <Route exact path="/login" component={LoginForm} />
           <Route exact path="/logout" component={Logout} />
         </Switch>
       </div>
---
+++ src/components/Forms/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/user/actions";
+
+export default function LoginFormContainer() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        email:
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <br />
+        password:
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <br />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+}
